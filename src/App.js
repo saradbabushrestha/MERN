e079001f082c9ad4c696f2b268dce4f38509ef49
Sarad Babu Shrestha <saradbabushrestha@Sarads-MacBook-Air.local>
2023-07-09 08:47:58 +0545

    MERN

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+//import Home from './components/Home';
 
 function App() {
   return (
-  <Login/>
+   
+    <Login/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,18 +9,9 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  );
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-//import React from "react";
-//import ReactDOM, { flushSync } from "react-dom";
-
-//ReactDOM.render(<h1>Hello world!!!</h1>,//jsx ko karan h1 use garna milyo k natra lamo arkai code lekna parxa babel ley convert arxa yo code lai 
-                                         //lamo huncha code use garena bane pure js ma garna koje no babel jsx
-    //document.getElementById("root"));
-    //DOM= document object model
-//react component ley break garxauxa  interface laiso that it can be handle independently
-//functional comp-ui or for preesentation only
