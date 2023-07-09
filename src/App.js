a2e5f53a56bf236b8c00f57fc9cd0937aedffe00
Sarad Babu Shrestha <saradbabushrestha@Sarads-MacBook-Air.local>
2023-07-09 10:18:42 +0545

    creating various components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Login from './components/Login';
-//import Home from './components/Home';
+import Home from './components/Home';
 
 function App() {
   return (
    
-    <Login/>
+    <Home/>
   );
 }
 
