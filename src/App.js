a91e306b92bbefa9ceb26711df35902d17e6688b
Sarad Babu Shrestha <saradbabushrestha@Sarads-MacBook-Air.local>
2023-08-12 09:18:13 +0545

    job post

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
-import Login from "./components/Login";
-import Home from "./components/Home";
 import { Component } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 class App extends Component {
   constructor() {
@@ -12,12 +12,26 @@ class App extends Component {
     };
     this.loginUser = this.loginUser.bind(this);
   }
+
   loginUser(user) {
     this.setState({ userLoggedIn: true, user: user });
+    window.sessionStorage.setItem("loggedInUser", JSON.stringify(user));
+    window.sessionStorage.setItem("isLoggedIn", "true");
   }
+
   render() {
-    if (this.state.userLoggedIn) {
-      return <Home user={this.state.user} />;
+    let user = this.state.user;
+    let userLoggedIn = this.state.userLoggedIn;
+    if (!Object.keys(user).length || !userLoggedIn) {
+      user = window.sessionStorage.getItem("loggedInUser");
+      userLoggedIn = window.sessionStorage.getItem("isLoggedIn") === "true";
+      if (userLoggedIn) {
+        user = JSON.parse(user);
+      }
+    }
+
+    if (userLoggedIn) {
+      return <Home user={user} />;
     } else {
       return <Login loginUser={this.loginUser} />;
     }
